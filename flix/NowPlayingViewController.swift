1af8e95eb938cec010d1521c128059563e9432a3
kymwithay <hillkymberlee@gmail.com>
2018-02-02 20:17:07 -0500

    added UI table view

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
-
+class NowPlayingViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
@@ -36,7 +40,16 @@ class NowPlayingViewController: UIViewController {
             }
         }
         task.resume()
-
+    }
+    
+    func tableView(_ _tableView: UITableView,numberOfRowsInSection section: Int) ->
+        Int{
+            return 10
+    }
+    func tableView(_ _tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
