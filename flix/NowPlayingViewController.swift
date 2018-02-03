68ac6dde06229fd5fb336984f5bdb522e72101b0
kymwithay <hillkymberlee@gmail.com>
2018-02-02 19:02:23 -0500

    finished network request for movie titles

---
+++ flix/NowPlayingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  NowPlayingViewController.swift
+//  flix
+//
+//  Created by Kymberlee Hill on 2/2/18.
+//  Copyright © 2018 Kymberlee Hill. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, respone, error) in
+            
+            //this will run when network request returns
+            
+            if let error = error{
+                print(error.localizedDescription)
+                
+            }else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movies in movies{
+                    let title = movies["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
