3c8dbc0a399e4525bb57f21f454b01082beb628e
kymwithay <hillkymberlee@gmail.com>
2018-02-13 20:27:26 -0500

    added custom class for superhero

---
+++ flix/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Kymberlee Hill on 2/13/18.
+//  Copyright © 2018 Kymberlee Hill. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
---
+++ flix/SuperheroViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SuperheroViewController.swift
+//  flix
+//
+//  Created by Kymberlee Hill on 2/13/18.
+//  Copyright © 2018 Kymberlee Hill. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
